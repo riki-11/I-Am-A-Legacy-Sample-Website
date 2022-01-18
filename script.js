f8501deaa02ef7e0218561822f6065b352eaf056
riki lej <73643957+riki-11@users.noreply.github.com>
2022-01-18 17:21:54 +0000

    added all elements of revised index page except for donations & newsletter subscription

--- script.js
+++ script.js
@@ -45,10 +45,3 @@ function showDivs(n) {
   slides[slideIndex - 1].style.display = 'inline-block';
   dots[slideIndex - 1].className += " w3-white";
 }
-
-
-//just embed them in the website XD
-// ooOOO maybe that's a better choice cos it'll make it faster to load 
-// can ya send the link in the gc so we could maybe use it later on when we put videos?
-//https://developers.facebook.com/docs/plugins/embedded-video-player/
-//i found a way to get videos from facebook Xd without downloading  
